151a64fa413a0f69503a5af726eeb611ee53f7e5
Kamryn Chan <kchan24@students.polytechnic.org>
2023-01-30 09:55:21 -0800

    Initial commit

---
+++ Pen.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Pen here.
+ *
+ * @author Sharon Chou & Kamryn Chan
+ * @version 2023-01-30
+ */
+public class Pen
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Pen
+     */
+    public Pen()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
